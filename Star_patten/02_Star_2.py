bab6193455ca7370d5d0b10c5c6a50976ef54e4a
abhaypanchal5 <abhaypanchal9015@gmail.com>
2026-02-11 20:27:27 +0530

    Star_patten/02_Star_2.py

--- Star_patten/02_Star_2.py
+++ Star_patten/02_Star_2.py
@@ -1,2 +1,7 @@
-n = int(input("Enter the number of row you want in your piramid"))
+n = int(input("Enter the number of row you want in your piramid: "))
 
+i = 1
+
+while i <= n:
+    print(" "*(n-i + 1),"*"*i + "*"*(i-1))
+    i += 1
